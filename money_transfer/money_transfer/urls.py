4ebcd5e36346d59470a68dfd2d98db084f6a0ff5
Kirill <vekgaming@yandex.ru>
2025-05-11 00:58:44 +0400

    fix: issues

--- money_transfer/money_transfer/urls.py
+++ money_transfer/money_transfer/urls.py
@@ -3,16 +3,17 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('home/', include('homepage.urls')),
     path('chaining/', include('smart_selects.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
+    path('', admin.site.urls),
 ]
 
 if settings.DEBUG:
     urlpatterns = [
-        *urlpatterns,
         path(
             '__debug__/',
             include('debug_toolbar.urls'),
         ),
+        *urlpatterns,
     ]
