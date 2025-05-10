7038c4d1a172f7c0b3e59710057fbc364e1de6b5
Kirill <vekgaming@yandex.ru>
2025-05-11 00:58:44 +0400

    fix: issues

--- money_transfer/core/validators.py
+++ money_transfer/core/validators.py
@@ -10,8 +10,10 @@ from django.utils.translation import gettext_lazy as _
 def validate_date_future(value):
     if isinstance(value, datetime.date):
         value = datetime.datetime.combine(
-            value, datetime.time.min, timezone.get_current_timezone(),
+            value,
+            datetime.time.min,
+            timezone.get_current_timezone(),
         )
 
-    if value > timezone.now() + datetime.timedelta(seconds=1):
+    if value > timezone.now() + datetime.timedelta(days=1):
         raise ValidationError(_('Time needs to be in past time'))
